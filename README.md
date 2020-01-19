a39d0e6531083314a42300bbc76e53b19f3faba6
Risenio <Risenio@users.noreply.github.com>
2020-01-19 22:49:24 +0400

    Patch 88 update.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A TERA utility to auto loot the items on the ground, as soon as they drop or wit
 
 * **!loot**
     * **Arguments:** auto, on, off, trash
+    * if no arguments, it will toggle auto.
 
 * **!loot auto:** 
     * Toggles auto loot on and off.
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+'use strict'
+
 const config = require('./config.json'),
 	blacklist = config.blacklist.concat(config.motes, config.strongboxes),
 	trash = config.trash.concat(config.crystals, config.strongboxes)
 
 module.exports = function Loot(mod) {
-	const {command} = mod.require
+	const { command, game } = mod.require,
+		{ inventory } = game
 
 	let auto = config.modes.auto || false,
 		autotrash = config.modes.trash || false,
@@ -13,11 +16,10 @@ module.exports = function Loot(mod) {
 		playerId = -1,
 		myLoc = null,
 		mounted = false,
-		inven = null,
 		loot = new Map(),
 		lootTimeout = null
 
-	let commands = {
+	const commands = {
 		auto: {
 			alias: ['auto', 'autoloot', 'toggle'],
 			run: function() {
@@ -25,7 +27,7 @@ module.exports = function Loot(mod) {
 				command.message(`Autoloot mode toggled: ${auto}`)
 				if(auto && !lootTimeout) tryLoot()
 				else {
-					clearTimeout(lootTimeout)
+					mod.clearTimeout(lootTimeout)
 					lootTimeout = null
 				}
 			}
@@ -48,17 +50,19 @@ module.exports = function Loot(mod) {
 			alias: ['autotrash', 'trash'],
 			run: function() {
 				autotrash = !autotrash
-
+				if(autotrash) inventory.off('update', inventoryUpdate).on('update', inventoryUpdate)
+				else inventory.off('update', inventoryUpdate)
 				command.message('Autotrash toggled: ' + (autotrash ? 'on' : 'off'))
 			}
 		}
 	}
 
-	mod.hook('S_LOGIN', mod.patchVersion < 81 ? 12 : 13, event => { ({gameId, playerId} = event) })
+	mod.hook('S_LOGIN', 14, event => { ({gameId, playerId} = event) })
 
 	command.add('loot', c => {
-		if(c)
-			for(let cmd in commands)
+		if(!c) commands.auto.run()
+		else
+			for(const cmd in commands)
 				if(commands[cmd].alias.includes(c))
 					commands[cmd].run()
 	})
@@ -75,43 +79,32 @@ module.exports = function Loot(mod) {
 	mod.hook('S_MOUNT_VEHICLE', 2, event => { if(event.gameId === gameId) mounted = true })
 	mod.hook('S_UNMOUNT_VEHICLE', 2, event => { if(event.gameId === gameId) mounted = false })
 
-	mod.hook('S_SPAWN_DROPITEM', mod.patchVersion < 80 ? 6 : 7, event => {
-		if(event.owners.some(owner => owner.playerId === playerId) && !blacklist.includes(event.item)) {
-			loot.set(event.gameId, Object.assign(event, {priority: 0}))
+	mod.hook('S_SPAWN_DROPITEM', 8, event => {
+		if(event.owners.indexOf(playerId) !== -1 && !blacklist.includes(event.item)) {
+			loot.set(event.gameId, Object.assign(event, { priority: 0 }))
 
 			if(auto && !lootTimeout) tryLoot()
 		}
 	})
 
-	mod.hook('C_TRY_LOOT_DROPITEM', 4, event => {
-		if(enabled && !lootTimeout) lootTimeout = setTimeout(tryLoot, config.lootInterval)
+	mod.hook('C_TRY_LOOT_DROPITEM', 'raw', () => {
+		if(enabled && !lootTimeout) lootTimeout = mod.setTimeout(tryLoot, config.lootInterval)
 	})
 
 	mod.hook('S_DESPAWN_DROPITEM', 4, event => { loot.delete(event.gameId) })
 
-	mod.hook('S_INVEN', mod.patchVersion < 80 ? 17 : 18, event => {
-		inven = event.first ? event.items : inven.concat(event.items)
-
-		if(!event.more) {
-			if(autotrash)
-				for(let item of inven)
-					if(item.slot < 40) continue // First 40 slots are reserved for equipment, etc.
-					else if(trash.includes(item.id)) deleteItem(item.slot, item.amount)
-
-			inven = null
+	const inventoryUpdate = () => {
+		if (!autotrash) {
+			inventory.off('update', inventoryUpdate)
+			return
 		}
-	})
-
-	function deleteItem(slot, amount) {
-		mod.toServer('C_DEL_ITEM', 2, {
-			gameId,
-			slot: slot - 40,
-			amount
-		})
+		for (const id of trash)
+			for (const item of inventory.findAll(id))
+				inventory.delete(item)
 	}
 
 	function tryLoot() {
-		clearTimeout(lootTimeout)
+		mod.clearTimeout(lootTimeout)
 		lootTimeout = null
 
 		if(!loot.size) return
@@ -120,10 +113,10 @@ module.exports = function Loot(mod) {
 			for(let l of [...loot.values()].sort((a, b) => a.priority - b.priority))
 				if(myLoc.dist3D(l.loc) <= config.lootRadius) {
 					mod.toServer('C_TRY_LOOT_DROPITEM', 4, l)
-					lootTimeout = setTimeout(tryLoot, Math.min(config.lootInterval * ++l.priority, config.lootThrottleMax))
+					lootTimeout = mod.setTimeout(tryLoot, Math.min(config.lootInterval * ++l.priority, config.lootThrottleMax))
 					return
 				}
 
-		if(auto) setTimeout(tryLoot, config.lootScanInterval)
+		if(auto) mod.setTimeout(tryLoot, config.lootScanInterval)
 	}
 }
